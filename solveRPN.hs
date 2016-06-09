6e3a3c08784c5e7e9a1a6ce08e3afc64dd6771c5
Rodrigo Morais <morais.rm@gmail.com>
2016-06-09 11:09:27 -0300

    Adding division to new RPN solution.

--- solveRPN.hs
+++ solveRPN.hs
@@ -1,6 +1,7 @@
 solveRPN :: String -> Double
 solveRPN = head . foldl foldingFunction [] . words
-  where foldingFunction (x:y:ys) "*" = (x * y) : ys
+  where foldingFunction (x:y:ys) "/" = (x / y) : ys
+        foldingFunction (x:y:ys) "*" = (x * y) : ys
         foldingFunction (x:y:ys) "+" = (x + y) : ys
         foldingFunction (x:y:ys) "-" = (x - y) : ys
         foldingFunction xs numberString = read numberString : xs
