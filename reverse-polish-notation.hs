7054454c913a3b1d7059476fb924666cc42e854e
Rodrigo Morais <morais.rm@gmail.com>
2016-06-09 09:59:17 -0300

    Adding the subtraction operation.

--- reverse-polish-notation.hs
+++ reverse-polish-notation.hs
@@ -1,29 +1,43 @@
+
 import Data.Char
 
-calculate :: String -> Int
+calculate :: [String] -> Int
 calculate [] = 0
-calculate (x:[]) = digitToInt x
+calculate (x:[]) = read x
 calculate (x:xs)
-  | isDigit x = calculate $ xs ++ [x]
-  | '+' == x = calculate $ (dropLastTwo xs) ++ [sumLastTwo xs]
-  | '*' == x = calculate $ (dropLastTwo xs) ++ [timesLastTwo xs]
+  | isValidNumber x = calculate $ xs ++ [x]
+  | "+" == x = calculate $ (dropLastTwo xs) ++ [sumLastTwo xs]
+  | "-" == x = calculate $ (dropLastTwo xs) ++ [subtractionLastTwo xs]
+  | "*" == x = calculate $ (dropLastTwo xs) ++ [timesLastTwo xs]
   | otherwise = 0
 
-sumLastTwo :: String -> Char
+sumLastTwo :: [String] -> String
 sumLastTwo xs =
-  intToDigit $
-  (+) (takeFirst xs) (takeSecond xs)
+  show $
+  (+) (takeSecond xs) (takeFirst xs)
 
-timesLastTwo :: String -> Char
+subtractionLastTwo :: [String] -> String
+subtractionLastTwo xs =
+  show $
+  (-) (takeSecond xs) (takeFirst xs)
+
+timesLastTwo :: [String] -> String
 timesLastTwo xs =
-  intToDigit $
-  (*) (takeFirst xs) (takeSecond xs)
+  show $
+  (*) (takeSecond xs) (takeFirst xs)
 
-dropLastTwo :: String -> String
+dropLastTwo :: [String] -> [String]
 dropLastTwo xs = reverse $ drop 2 $ reverse xs
 
-takeFirst :: String -> Int
-takeFirst xs = digitToInt $ head $ take 1 $ reverse xs
+takeFirst :: [String] -> Int
+takeFirst xs = read $ head $ take 1 $ reverse xs
 
-takeSecond :: String -> Int
-takeSecond xs = digitToInt $ head $ reverse $ take 2 $ reverse xs
+takeSecond :: [String] -> Int
+takeSecond xs = read $ head $ reverse $ take 2 $ reverse xs
+
+isValidNumber :: String -> Bool
+isValidNumber [] = False
+isValidNumber (x:[]) = if isDigit x then True else False
+isValidNumber (x:xs)
+  | isDigit x = True && isValidNumber xs
+  | otherwise = False 
