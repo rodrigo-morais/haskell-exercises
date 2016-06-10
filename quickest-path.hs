0add53eb12604f60d809fc4c145e14af8d40116b
Rodrigo Morais <morais.rm@gmail.com>
2016-06-10 15:48:16 -0300

    Adding the solution to quickest path.

--- quickest-path.hs
+++ quickest-path.hs
@@ -7,4 +7,32 @@ data Label = A | B | C
 type Path = [(Label, Int)]
 
 optimalPath :: RoadSystem -> Path
-optimalPath road = [(A, 1)]
+optimalPath road =
+  let (bestPathA, bestPathB) = foldl roadStep ([], []) road
+  in  if sum (map snd bestPathA) <= sum (map snd bestPathB)
+        then reverse bestPathA
+        else reverse bestPathB
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+  let timeA = sum (map snd pathA)
+      timeB = sum (map snd pathB)
+      forwardTimeToA = timeA + a
+      forwardTimeToB = timeB + b
+      crossTimeToA = timeB + b + c
+      crossTimeToB = timeA + a + c
+      newPathToA = if forwardTimeToA <= crossTimeToA
+                    then (A, a) : pathA
+                    else (C, c) : (B, b) : pathB
+      newPathToB = if forwardTimeToB <= crossTimeToB
+                    then (B, b) : pathB
+                    else (C, c) : (A, a) : pathA
+  in
+    (newPathToA, newPathToB)
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon =  [ Section 50 10 30
+                    , Section 5 90 20
+                    , Section 40 2 25
+                    , Section 10 8 0
+                    ]
