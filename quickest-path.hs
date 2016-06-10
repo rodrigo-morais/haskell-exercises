3cf31513b9ed684e3388aa074215494c8b33eeba
Rodrigo Morais <morais.rm@gmail.com>
2016-06-10 13:56:56 -0300

    Creating the Label and Path types, and the optimalPath.

--- quickest-path.hs
+++ quickest-path.hs
@@ -1,4 +1,10 @@
 data Section = Section { getA :: Int, getB :: Int, getC :: Int }
   deriving Show
-
 type RoadSystem = [Section]
+
+data Label = A | B | C
+  deriving (Show)
+type Path = [(Label, Int)]
+
+optimalPath :: RoadSystem -> Path
+optimalPath road = [(A, 1)]
